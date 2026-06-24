848fe195fb17d3904c5f8619dd0892d83969f1bd
Tolay Khamis <Tolaykh@outlook.com>
2026-06-24 03:52:45 +0300

    update

--- lib/constants/app_colors.dart
+++ lib/constants/app_colors.dart
@@ -1,17 +1,16 @@
-// constants/app_colors.dart
 import 'package:flutter/material.dart';
 
 class AppColors {
   static const Color primary     = Color(0xFF062C2B);
-  static const Color secondary   = Color(0xFF0F766E); 
-  static const Color accent      = Color(0xFF14B8A6); 
+  static const Color secondary   = Color(0xFF0F766E);
+  static const Color accent      = Color(0xFF14B8A6);
   static const Color background  = Color(0xFFF4F7F6);
   static const Color cardBg      = Colors.white;
-  static const Color pending     = Color(0xFFFEF3C7); 
+  static const Color pending     = Color(0xFFFEF3C7);
   static const Color pendingText = Color(0xFFB45309);
   static const Color inProgress  = Color(0xFFE0F2FE);
   static const Color inProgressText = Color(0xFF0369A1);
-  static const Color completed   = Color(0xFFD1FAE5); 
+  static const Color completed   = Color(0xFFD1FAE5);
   static const Color completedText  = Color(0xFF047857);
   static const Color cancelled   = Color(0xFFFFE4E6);
   static const Color cancelledText  = Color(0xFFBE123C);
